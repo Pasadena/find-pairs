7a5f42789b228ea5861e46aedf5f392e1a2213fb
Simo-Pekka Koskinen <simo.pekka.koskinen@gmail.com>
2022-05-04 18:16:51 +0300

    Removed generated content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,14 +12,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-	</h1>
+	<h1>Etsi parit</h1>
 
 	<GameBoard />
 </section>
@@ -35,20 +28,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
